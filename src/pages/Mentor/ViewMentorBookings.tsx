a62d991e4b2aa0cc3928809b0abcfb1f75d73978
ahmadit575 <ahmadit575@gmail.com>
2023-11-11 21:17:48 +0500

    booking view scroll

--- src/pages/Mentor/ViewMentorBookings.tsx
+++ src/pages/Mentor/ViewMentorBookings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
@@ -12,18 +12,18 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from '@mui/material';
-import styled from 'styled-components';
-import clockIcon from '../../Assets/Images/clock.png';
-import personImg from '../../Assets/Images/person.jpeg';
-import { BsEye } from 'react-icons/bs';
-import Cancel from '@mui/icons-material/Cancel';
-import Visibility from '@mui/icons-material/Visibility';
-import { Box } from '@mui/system';
-import { formatDate } from '../../helper-functions';
-import { StatusMentorBooking } from './StatusMentorBookings';
-import { changesBookingStatus } from '../../api';
-import { notifyError, notifySuccess } from '../../components/Toastifycom';
+} from "@mui/material";
+import styled from "styled-components";
+import clockIcon from "../../Assets/Images/clock.png";
+import personImg from "../../Assets/Images/person.jpeg";
+import { BsEye } from "react-icons/bs";
+import Cancel from "@mui/icons-material/Cancel";
+import Visibility from "@mui/icons-material/Visibility";
+import { Box } from "@mui/system";
+import { formatDate } from "../../helper-functions";
+import { StatusMentorBooking } from "./StatusMentorBookings";
+import { changesBookingStatus } from "../../api";
+import { notifyError, notifySuccess } from "../../components/Toastifycom";
 
 export const ViewMentorBooking = ({
   open,
@@ -37,20 +37,20 @@ export const ViewMentorBooking = ({
   getAllBookings: Function;
 }): JSX.Element => {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const [reason, setReason] = useState('');
+  const [reason, setReason] = useState("");
   const [statusModal, setStatusModal] = useState(false);
 
   const handleStatusChange = () => {
     changesBookingStatus({
       bookingId: data?._id,
-      newBookingStatus: 'COMPLETED',
+      newBookingStatus: "COMPLETED",
       message: reason,
     })
       .then((res) => {
-        notifySuccess('Booking Accepted Successfully!');
-        getAllBookings('ALL');
+        notifySuccess("Booking Accepted Successfully!");
+        getAllBookings("ALL");
         setStatusModal(false);
         setShowDetails(false);
       })
@@ -60,120 +60,117 @@ export const ViewMentorBooking = ({
   };
   return (
     <>
-      <Dialog open={open} maxWidth={'xl'}>
+      <Dialog open={open} maxWidth={"xl"}>
         <DialogContent>
-          <Box
-            display="grid"
-            gridTemplateColumns={
-              !isMobile ? 'repeat(2, 2fr)' : 'repeat(2, 2fr)'
-            }
-            gap={2}
-          >
-            <Stack
-              sx={{
-                color: 'black',
-                borderBottom: '3px solid #5F61BE',
+          <Grid container>
+            <Grid item sm={12} lg={12}>
+              <Grid container justifyContent={"space-between"} spacing={1}>
+                <Grid
+                  item
+                  sm={6}
+                  lg={6}
+                  sx={{
+                    color: "black",
+                    borderBottom: "3px solid #5F61BE",
+                    width: "fit-content",
+                  }}
+                  direction={"row"}
+                >
+                  <Typography fontWeight={700} noWrap>
+                    {data?.bookingSubject}
+                  </Typography>
+                </Grid>
 
-                gridColumn: 'span 6',
-                width: 'fit-content',
-              }}
-              direction={'row'}
-            >
-              <Typography fontWeight={700} noWrap>
-                {data?.bookingSubject}
-              </Typography>
-            </Stack>
-            <Stack
-              alignItems={'flex-end'}
-              justifyContent={'center'}
-              sx={{
-                gridColumn: 'span 6',
-              }}
-            >
-              <Typography fontWeight={600}>$ {data?.amount}</Typography>
-            </Stack>
+                <Grid item sm={6} lg={6}>
+                  <Typography fontWeight={600} textAlign={"right"}>
+                    $ {data?.amount}
+                  </Typography>
+                </Grid>
 
-            <Stack sx={{ gridColumn: 'span 12' }}>
-              <Typography
-                textAlign={'left'}
-                color={'#7A7A7A'}
-                fontSize={'small'}
-              >
-                {data?.description}
-              </Typography>
-            </Stack>
-            <Stack
-              flexDirection={'column'}
-              sx={
-                !isMobile ? { gridColumn: 'span 6' } : { gridColumn: 'span 12' }
-              }
-            >
-              <Stack
-                sx={{
-                  color: 'black',
-                  borderBottom: '3px solid #5F61BE',
-
-                  mt: 2,
-                  mb: 2,
-                  width: 'fit-content',
-                }}
-              >
-                <Typography fontWeight={700}>Date & Time</Typography>
-              </Stack>
-              <Typography
-                textAlign={'left'}
-                fontSize={'small'}
-                color={'#7A7A7A'}
-              >
-                {formatDate(data?.bookingDate, '')}
-              </Typography>
-              <Typography
-                textAlign={'left'}
-                fontSize={'small'}
-                color={'#7A7A7A'}
-              >
-                {data?.time}
-              </Typography>
-              <Typography
-                textAlign={'left'}
-                fontSize={'small'}
-                color={'#7A7A7A'}
-              >
-                {data?.duration} Minutes
-              </Typography>
-            </Stack>
-
-            <Stack flexDirection={'column'}>
-              <Stack
-                sx={{
-                  color: 'black',
-                  borderBottom: '3px solid #5F61BE',
-
-                  mt: 2,
-                  mb: 2,
-                  width: 'fit-content',
-                }}
-              >
-                <Typography fontWeight={700}>Student Information</Typography>
-              </Stack>
-              <Stack flexDirection={'row'}>
-                <Avatar />
-                <Stack flexDirection={'column'} sx={{ ml: 1 }}>
+                <Grid item sm={12} lg={12}>
                   <Typography
-                    noWrap
-                    variant="h6"
-                    fontSize={'medium'}
-                    fontWeight={600}
+                    textAlign={"left"}
+                    color={"#7A7A7A"}
+                    fontSize={"small"}
                   >
-                    {data?.student[0]?.first_name + data?.student[0]?.last_name}
+                    {data?.description}
                   </Typography>
-                  <Typography noWrap color={'#7A7A7A'}>
-                    {data?.student[0]?.email}
-                  </Typography>
-                </Stack>
-              </Stack>
-            </Stack>
-          </Box>
+                </Grid>
+                <Grid item sm={12} lg={6}>
+                  <Stack flexDirection={"column"}>
+                    <Stack
+                      sx={{
+                        color: "black",
+                        borderBottom: "3px solid #5F61BE",
+
+                        mt: 2,
+                        mb: 2,
+                        width: "fit-content",
+                      }}
+                    >
+                      <Typography fontWeight={700}>Date & Time</Typography>
+                    </Stack>
+                    <Typography
+                      textAlign={"left"}
+                      fontSize={"small"}
+                      color={"#7A7A7A"}
+                    >
+                      {formatDate(data?.bookingDate, "")}
+                    </Typography>
+                    <Typography
+                      textAlign={"left"}
+                      fontSize={"small"}
+                      color={"#7A7A7A"}
+                    >
+                      {data?.time}
+                    </Typography>
+                    <Typography
+                      textAlign={"left"}
+                      fontSize={"small"}
+                      color={"#7A7A7A"}
+                    >
+                      {data?.duration} Minutes
+                    </Typography>
+                  </Stack>
+                </Grid>
+                <Grid item sm={12} lg={6}>
+                  <Stack flexDirection={"column"}>
+                    <Stack
+                      sx={{
+                        color: "black",
+                        borderBottom: "3px solid #5F61BE",
+
+                        mt: 2,
+                        mb: 2,
+                        width: "fit-content",
+                      }}
+                    >
+                      <Typography fontWeight={700}>
+                        Student Information
+                      </Typography>
+                    </Stack>
+                    <Stack flexDirection={"row"}>
+                      <Avatar />
+                      <Stack flexDirection={"column"} sx={{ ml: 1 }}>
+                        <Typography
+                          noWrap
+                          variant="h6"
+                          fontSize={"medium"}
+                          fontWeight={600}
+                        >
+                          {data?.student[0]?.first_name +
+                            data?.student[0]?.last_name}
+                        </Typography>
+                        <Typography noWrap color={"#7A7A7A"}>
+                          {data?.student[0]?.email}
+                        </Typography>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           {data?.bookingStatus !== 'COMPLETED' &&
