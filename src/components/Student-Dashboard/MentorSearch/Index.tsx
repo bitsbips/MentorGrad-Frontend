03ab7732c59dafb98408e0a3166d2a7a9d46c52b
ahmadit575 <ahmadit575@gmail.com>
2023-11-21 22:11:46 +0500

    chnges

--- src/components/Student-Dashboard/MentorSearch/Index.tsx
+++ src/components/Student-Dashboard/MentorSearch/Index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { ContainerDashboard } from '../StudentDahboardStyles';
+import React, { useEffect, useState } from "react";
+import { ContainerDashboard } from "../StudentDahboardStyles";
 import {
   PositionHeader,
   PositionImage,
   RightBorderDashboard,
   RightContainerDash,
-} from '../../StudentDashboard/StudentDashboardStyles';
+} from "../../StudentDashboard/StudentDashboardStyles";
 import {
   Box,
   Button,
@@ -18,23 +18,23 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from '@mui/material';
-import SearchFilter from './SearchFilter';
-import { GetCountryList, findMentors } from '../../../api';
-import { notifyError } from '../../Toastifycom';
-import picture from '../../../Assets/Images/user.jpeg';
-import StarIcon from '@mui/icons-material/Star';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import ReviewsIcon from '@mui/icons-material/Reviews';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import MoneyIcon from '@mui/icons-material/Money';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import Spinner from '../../Spinner';
-import { Container } from '../../../pages/AuthFlow/AuthStyles';
-import Footer from '../../Footer';
-import { ContainerDa } from '../../StudentProfileDetails/StudentProfileStyles';
-import HeaderDashboard from '../../Header/HeaderDashboard';
-import { useNavigate } from 'react-router-dom';
+} from "@mui/material";
+import SearchFilter from "./SearchFilter";
+import { GetCountryList, findMentors } from "../../../api";
+import { notifyError } from "../../Toastifycom";
+import picture from "../../../Assets/Images/user.jpeg";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import ReviewsIcon from "@mui/icons-material/Reviews";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import MoneyIcon from "@mui/icons-material/Money";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import Spinner from "../../Spinner";
+import { Container } from "../../../pages/AuthFlow/AuthStyles";
+import Footer from "../../Footer";
+import { ContainerDa } from "../../StudentProfileDetails/StudentProfileStyles";
+import HeaderDashboard from "../../Header/HeaderDashboard";
+import { useNavigate } from "react-router-dom";
 
 type Mentor = {
   _id: string;
@@ -44,10 +44,12 @@ type Mentor = {
   lastName: string;
   countryOfResidence: string;
   hourlyRate: number;
-  attachments: Array<{
-    attachmentPath: string;
-    attachmentURL: string;
-    name: string;
+  user: Array<{
+    attachments: Array<{
+      attachmentPath: string;
+      attachmentURL: string;
+      name: string;
+    }>;
   }>;
 };
 
@@ -56,14 +58,14 @@ type MentorList = Mentor[];
 const MentorSearch = () => {
   const theme = useTheme();
   const navigate = useNavigate();
-  const isMobile = useMediaQuery('(max-width: 768px)');
-  const isMobile2 = useMediaQuery('(max-width: 1200px)');
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isMobile2 = useMediaQuery("(max-width: 1200px)");
   const [showFilter, setshowFilter] = useState(false);
   const [filters, setFilters] = useState({
-    country: '',
-    gender: '',
-    course: '',
-    location: '',
+    country: "",
+    gender: "",
+    course: "",
+    location: "",
   });
   const [countries, setCountries] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
@@ -139,29 +141,29 @@ const MentorSearch = () => {
         <ContainerDa>
           <ContainerDashboard>
             <Stack
-              flexDirection={'row'}
-              alignItems={'center'}
-              justifyContent={'space-between'}
+              flexDirection={"row"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
             >
               <Typography
                 sx={{ mt: 1, mb: 2.5 }}
-                textAlign={'left'}
+                textAlign={"left"}
                 fontWeight={700}
-                fontSize={isMobile ? 'small' : 'large'}
+                fontSize={isMobile ? "small" : "large"}
                 noWrap
               >
-                {mentorList.length} matches found for: Mentors{' '}
-                {filters.country === '' || filters.country === null
-                  ? 'OverAll'
-                  : ` in 
-            ${mentorList[0]?.countryOfResidence}`}
+                {mentorList.length} matches found for: Mentors{" "}
+                {filters.country === "" || filters.country === null
+                  ? "OverAll"
+                  : filters.country === mentorList[0]?.countryOfResidence ? ` in 
+            ${mentorList[0]?.countryOfResidence}` : "OverAll"}
               </Typography>
               {isMobile2 && (
                 <IconButton
                   onClick={() => setshowFilter(true)}
                   sx={{ p: 0, m: 0, mb: 1 }}
                 >
-                  <FilterAltIcon fontSize="small" sx={{ color: '#5F61BE' }} />
+                  <FilterAltIcon fontSize="small" sx={{ color: "#5F61BE" }} />
                 </IconButton>
               )}
             </Stack>
@@ -186,56 +188,58 @@ const MentorSearch = () => {
                       <Spinner />
                     ) : (
                       mentorList?.map((mentor, index) => (
-                        <Box sx={{ margin: isMobile ? '0px 15px' : '' }}>
+                        <Box sx={{ margin: isMobile ? "0px 15px" : "" }}>
                           <RightBorderDashboard>
                             <Stack
-                              justifyContent={'space-between'}
-                              flexDirection={isMobile ? 'column' : 'row'}
+                              justifyContent={"space-between"}
+                              flexDirection={isMobile ? "column" : "row"}
                               gap={isMobile ? 3 : 0}
                               p={isMobile ? 2.5 : 0}
                             >
                               <Stack
-                                flexDirection={'row'}
+                                flexDirection={"row"}
                                 gap={2}
-                                width={isMobile ? '100%' : '70%'}
+                                width={isMobile ? "100%" : "70%"}
                               >
                                 <img
                                   style={
                                     isMobile
-                                      ? { width: '35%', borderRadius: '10px' }
-                                      : { width: '20%', borderRadius: '10px' }
+                                      ? { width: "35%", borderRadius: "10px" }
+                                      : { width: "20%", borderRadius: "10px" }
                                   }
                                   src={
-                                    mentor?.attachments[0]?.attachmentPath ||
-                                    picture
+                                    mentor?.user[0]?.attachments
+                                      ? mentor?.user[0]?.attachments[0]
+                                          ?.attachmentPath
+                                      : picture
                                   }
                                 />
-                                <Stack flexDirection={'column'}>
+                                <Stack flexDirection={"column"}>
                                   <Typography
-                                    textAlign={'left'}
+                                    textAlign={"left"}
                                     noWrap
-                                    sx={{ color: '#5F61BE' }}
-                                    fontSize={'medium'}
+                                    sx={{ color: "#5F61BE" }}
+                                    fontSize={"medium"}
                                     fontWeight={600}
                                   >
                                     {mentor?.userName}
                                   </Typography>
                                   <Typography
-                                    textAlign={'left'}
+                                    textAlign={"left"}
                                     noWrap
-                                    fontSize={'small'}
-                                    sx={{ color: '#8E8E8E' }}
+                                    fontSize={"small"}
+                                    sx={{ color: "#8E8E8E" }}
                                   >
                                     Data Scientist
                                   </Typography>
                                   <Stack
-                                    flexDirection={'row'}
+                                    flexDirection={"row"}
                                     sx={{ mt: 2 }}
-                                    alignItems={'center'}
+                                    alignItems={"center"}
                                   >
                                     {new Array(4).fill(
                                       <StarIcon
-                                        sx={{ color: '#FFD707' }}
+                                        sx={{ color: "#FFD707" }}
                                         fontSize="small"
                                       />
                                     )}
@@ -245,19 +249,19 @@ const MentorSearch = () => {
                                     <small>(17)</small>
                                   </Stack>
                                   <Typography
-                                    textAlign={'left'}
-                                    sx={{ color: '#757575' }}
-                                    fontSize={'small'}
+                                    textAlign={"left"}
+                                    sx={{ color: "#757575" }}
+                                    fontSize={"small"}
                                   >
                                     {mentor.countryOfResidence}
                                   </Typography>
                                 </Stack>
                               </Stack>
 
-                              <Stack flexDirection={'column'} gap={1}>
+                              <Stack flexDirection={"column"} gap={1}>
                                 <Stack
-                                  flexDirection={'row'}
-                                  alignItems={'flex-end'}
+                                  flexDirection={"row"}
+                                  alignItems={"flex-end"}
                                   gap={1}
                                 >
                                   <ReviewsIcon fontSize="small" />
@@ -267,8 +271,8 @@ const MentorSearch = () => {
                                 </Stack>
 
                                 <Stack
-                                  flexDirection={'row'}
-                                  alignItems={'flex-end'}
+                                  flexDirection={"row"}
+                                  alignItems={"flex-end"}
                                   gap={1}
                                 >
                                   <LocationOnIcon fontSize="small" />
@@ -278,8 +282,8 @@ const MentorSearch = () => {
                                 </Stack>
 
                                 <Stack
-                                  flexDirection={'row'}
-                                  alignItems={'flex-end'}
+                                  flexDirection={"row"}
+                                  alignItems={"flex-end"}
                                   gap={1}
                                 >
                                   <MoneyIcon fontSize="small" />
@@ -299,16 +303,16 @@ const MentorSearch = () => {
                                   sx={
                                     isMobile
                                       ? {
-                                          background: '#5F61BE',
-                                          width: 'fit-content',
-                                          mt: '10px',
-                                          ml: '15vw',
-                                          px: '10px',
+                                          background: "#5F61BE",
+                                          width: "fit-content",
+                                          mt: "10px",
+                                          ml: "15vw",
+                                          px: "10px",
                                         }
                                       : {
-                                          background: '#5F61BE',
-                                          width: 'fit-content',
-                                          mt: '10px',
+                                          background: "#5F61BE",
+                                          width: "fit-content",
+                                          mt: "10px",
                                         }
                                   }
                                 >
@@ -328,8 +332,8 @@ const MentorSearch = () => {
             {showFilter && (
               <Dialog
                 open={showFilter}
-                maxWidth={'xl'}
-                sx={{ overflowX: 'scroll' }}
+                maxWidth={"xl"}
+                sx={{ overflowX: "scroll" }}
               >
                 <DialogContent>
                   <SearchFilter
