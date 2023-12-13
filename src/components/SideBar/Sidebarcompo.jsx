6e3d12f5e54482cacb88cbb75e1817de08e8f880
OssamaPuma <usama@devpumas.com>
2023-12-13 11:24:16 +0500

    Admin panel integrated

--- src/components/SideBar/Sidebarcompo.jsx
+++ src/components/SideBar/Sidebarcompo.jsx
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import Hidden from '@mui/material/Hidden';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { withStyles } from '@mui/styles';
-import HeaderUserinfo from '../StudentDashboard/HeaderUserinfo';
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import Hidden from "@mui/material/Hidden";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { withStyles } from "@mui/styles";
+import HeaderUserinfo from "../StudentDashboard/HeaderUserinfo";
 
 const styles = {
   drawer: {
     width: 250, // Set your desired width for big screens
-    margin: 'auto',
-    position: 'fixed', // Add this property
-
+    margin: "auto",
+    position: "fixed", // Add this property
   },
-
 };
-
-
 
 const Sidebarcompo = ({ classes }) => {
   const [mobileOpen, setMobileOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState('Home');
-  const isMobile = useMediaQuery('(max-width: 950px)');
+  const [selectedItem, setSelectedItem] = useState("Home");
+  const isMobile = useMediaQuery("(max-width: 950px)");
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -42,37 +38,51 @@ const Sidebarcompo = ({ classes }) => {
       <Drawer
         anchor="left"
         open={isMobile ? mobileOpen : true}
-        variant={isMobile ? 'temporary' : 'permanent'}
+        variant={isMobile ? "temporary" : "permanent"}
         onClose={isMobile ? handleDrawerToggle : undefined}
         classes={{
           paper: classes.drawer, // Apply custom width to the sidebar
         }}
         PaperProps={{
           sx: {
-            height: isMobile ? '' : 500,
-            top: isMobile ? '' : 64,
-
-          }
+            height: isMobile ? "" : 500,
+            top: isMobile ? "" : 64,
+          },
         }}
       >
         <List>
-          <ListItem onClick={() => handleListItemClick('Home')} className={selectedItem === 'Home' ? 'active' : ''}>
-            <HomeIcon className={selectedItem === 'Home' ? 'active-icon' : ''} />
+          <ListItem
+            onClick={() => handleListItemClick("Home")}
+            className={selectedItem === "Home" ? "active" : ""}
+          >
+            <HomeIcon
+              className={selectedItem === "Home" ? "active-icon" : ""}
+            />
             <ListItemText primary="Home" />
           </ListItem>
-          <ListItem onClick={() => handleListItemClick('About')} className={selectedItem === 'About' ? 'active' : ''}>
-            <InfoIcon className={selectedItem === 'About' ? 'active-icon' : ''} />
+          <ListItem
+            onClick={() => handleListItemClick("About")}
+            className={selectedItem === "About" ? "active" : ""}
+          >
+            <InfoIcon
+              className={selectedItem === "About" ? "active-icon" : ""}
+            />
             <ListItemText primary="About" />
           </ListItem>
-          <ListItem onClick={() => handleListItemClick('Services')} className={selectedItem === 'Services' ? 'active' : ''}>
-            <AssignmentIcon className={selectedItem === 'Services' ? 'active-icon' : ''} />
+          <ListItem
+            onClick={() => handleListItemClick("Services")}
+            className={selectedItem === "Services" ? "active" : ""}
+          >
+            <AssignmentIcon
+              className={selectedItem === "Services" ? "active-icon" : ""}
+            />
             <ListItemText primary="Services" />
           </ListItem>
           {/* Add more list items as needed */}
         </List>
       </Drawer>
-      <div style={{ flex: 1, padding: '16px' }}>
-        {selectedItem === 'Home' && <HeaderUserinfo />}
+      <div style={{ flex: 1, padding: "16px" }}>
+        {selectedItem === "Home" && <HeaderUserinfo />}
       </div>
       {isMobile && (
         <Hidden lgUp implementation="css">
---
+++ src/pages/Admin/menu-items/application.js
@@ -0,0 +1,305 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
+// assets
+import { IconApps, IconUserCheck, IconBasket, IconMessages, IconLayoutKanban, IconMail, IconCalendar, IconNfc } from '@tabler/icons';
+
+// constant
+const icons = {
+    IconApps,
+    IconUserCheck,
+    IconBasket,
+    IconMessages,
+    IconLayoutKanban,
+    IconMail,
+    IconCalendar,
+    IconNfc
+};
+
+// ==============================|| APPLICATION MENU ITEMS ||============================== //
+
+const application = {
+    id: 'application',
+    title: <FormattedMessage id="application" />,
+    icon: icons.IconApps,
+    type: 'group',
+    children: [
+        {
+            id: 'users',
+            title: <FormattedMessage id="users" />,
+            type: 'collapse',
+            icon: icons.IconUserCheck,
+            children: [
+                {
+                    id: 'posts',
+                    title: <FormattedMessage id="social-profile" />,
+                    type: 'item',
+                    // url: '/user/social-profile/posts'
+                    url: '/admin'
+
+                },
+                {
+                    id: 'account-profile',
+                    title: <FormattedMessage id="account-profile" />,
+                    type: 'collapse',
+                    children: [
+                        {
+                            id: 'profile1',
+                            title: (
+                                <>
+                                    <FormattedMessage id="profile" /> 01
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/account-profile/profile1'
+                            url: '/admin'
+                        },
+                        {
+                            id: 'profile2',
+                            title: (
+                                <>
+                                    <FormattedMessage id="profile" /> 02
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/account-profile/profile2'
+                            url: '/admin'
+                        },
+                        {
+                            id: 'profile3',
+                            title: (
+                                <>
+                                    <FormattedMessage id="profile" /> 03
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/account-profile/profile3'
+                            url: '/admin'
+                        }
+                    ]
+                },
+                {
+                    id: 'user-card',
+                    title: <FormattedMessage id="cards" />,
+                    type: 'collapse',
+                    children: [
+                        {
+                            id: 'card1',
+                            title: (
+                                <>
+                                    <FormattedMessage id="style" /> 01
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/card/card1'
+                            url: '/admin'
+                        },
+                        {
+                            id: 'card2',
+                            title: (
+                                <>
+                                    <FormattedMessage id="style" /> 02
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/card/card2'
+                            url: '/admin'
+                        },
+                        {
+                            id: 'card3',
+                            title: (
+                                <>
+                                    <FormattedMessage id="style" /> 03
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/card/card3'
+                            url: '/admin'
+                        }
+                    ]
+                },
+                {
+                    id: 'user-list',
+                    title: <FormattedMessage id="list" />,
+                    type: 'collapse',
+                    children: [
+                        {
+                            id: 'list1',
+                            title: (
+                                <>
+                                    <FormattedMessage id="style" /> 01
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/list/list1'
+                            url: '/admin'
+                        },
+                        {
+                            id: 'list2',
+                            title: (
+                                <>
+                                    <FormattedMessage id="style" /> 02
+                                </>
+                            ),
+                            type: 'item',
+                            // url: '/user/list/list2'
+                            url: '/admin'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: 'customer',
+            title: <FormattedMessage id="customer" />,
+            type: 'collapse',
+            icon: icons.IconBasket,
+            children: [
+                {
+                    id: 'customer-list',
+                    title: <FormattedMessage id="customer-list" />,
+                    type: 'item',
+                    // url: '/customer/customer-list',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'order-list',
+                    title: <FormattedMessage id="order-list" />,
+                    type: 'item',
+                    // url: '/customer/order-list',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'create-invoice',
+                    title: <FormattedMessage id="create-invoice" />,
+                    type: 'item',
+                    // url: '/customer/create-invoice',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'order-details',
+                    title: <FormattedMessage id="order-details" />,
+                    type: 'item',
+                    // url: '/customer/order-details'
+                    url: '/admin',
+                },
+                {
+                    id: 'product',
+                    title: <FormattedMessage id="product" />,
+                    type: 'item',
+                    // url: '/customer/product',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'product-review',
+                    title: <FormattedMessage id="product-review" />,
+                    type: 'item',
+                    // url: '/customer/product-review',
+                    url: '/admin',
+                    breadcrumbs: false
+                }
+            ]
+        },
+        {
+            id: 'chat',
+            title: <FormattedMessage id="chat" />,
+            type: 'item',
+            icon: icons.IconMessages,
+            // url: '/app/chat'
+            url: '/admin',
+        },
+        {
+            id: 'kanban',
+            title: 'Kanban',
+            type: 'item',
+            icon: icons.IconLayoutKanban,
+            // url: '/app/kanban/board'
+            url: '/admin',
+        },
+        {
+            id: 'mail',
+            title: <FormattedMessage id="mail" />,
+            type: 'item',
+            icon: icons.IconMail,
+            // url: '/app/mail'
+            url: '/admin',
+        },
+        {
+            id: 'calendar',
+            title: <FormattedMessage id="calendar" />,
+            type: 'item',
+            // url: '/app/calendar',
+            url: '/admin',
+            icon: icons.IconCalendar,
+            breadcrumbs: false
+        },
+        {
+            id: 'contact',
+            title: <FormattedMessage id="contact" />,
+            type: 'collapse',
+            icon: icons.IconNfc,
+            children: [
+                {
+                    id: 'c-card',
+                    title: <FormattedMessage id="cards" />,
+                    type: 'item',
+                    // url: '/app/contact/c-card',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'c-list',
+                    title: <FormattedMessage id="list" />,
+                    type: 'item',
+                    // url: '/app/contact/c-list',
+                    url: '/admin',
+                    breadcrumbs: false
+                }
+            ]
+        },
+        {
+            id: 'e-commerce',
+            title: <FormattedMessage id="e-commerce" />,
+            type: 'collapse',
+            icon: icons.IconBasket,
+            children: [
+                {
+                    id: 'products',
+                    title: <FormattedMessage id="products" />,
+                    type: 'item',
+                    // url: '/e-commerce/products'
+                    url: '/admin',
+                },
+                {
+                    id: 'product-details',
+                    title: <FormattedMessage id="product-details" />,
+                    type: 'item',
+                    // url: '/e-commerce/product-details/1',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'product-list',
+                    title: <FormattedMessage id="product-list" />,
+                    type: 'item',
+                    // url: '/e-commerce/product-list',
+                    url: '/admin',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'checkout',
+                    title: <FormattedMessage id="checkout" />,
+                    type: 'item',
+                    // url: '/e-commerce/checkout'
+                    url: '/admin',
+                }
+            ]
+        }
+    ]
+};
+
+export default application;
